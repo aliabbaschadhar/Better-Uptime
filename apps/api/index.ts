61becb35e6ef64489e27a6dffbf604e07ddf3c45
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-08-01 16:21:19 +0500

    routes re-arranged

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -3,6 +3,7 @@ import { prismaClient } from "store/client"
 import cors from "cors"
 import { authInput } from "./types"
 import jwt from "jsonwebtoken"
+import { authMiddleware } from "./middleware"
 
 const app = express()
 
@@ -23,30 +24,6 @@ app.get("/", (req, res) => {
     status: "OK",
     timestamp: new Date().toISOString(),
     service: "better-uptime-api"
-  })
-})
-
-app.post("/website", async (req, res) => {
-  const website = await prismaClient.website.create({
-    data: {
-      url: req.body.url,
-      timeAdded: new Date()
-    }
-  })
-  return res.json({
-    "msg": "On /website",
-    id: website.id
-  })
-})
-
-app.get("/status/:websiteId", (req, res) => {
-  const websiteId = req.params.websiteId
-  // Here you would typically check the status of the website with the given ID
-  // For now, we will just return a dummy status
-  return res.json({
-    "msg": "On /status",
-    websiteId: websiteId,
-    status: "up" // This is a placeholder; actual status checking logic would go here
   })
 })
 
@@ -138,6 +115,32 @@ app.post("/user/signin", async (req, res) => {
   });
 
 });
+
+
+app.post("/website", authMiddleware, async (req, res) => {
+  const website = await prismaClient.website.create({
+    data: {
+      url: req.body.url,
+      timeAdded: new Date()
+    }
+  })
+  return res.json({
+    "msg": "On /website",
+    id: website.id
+  })
+})
+
+app.get("/status/:websiteId", authMiddleware, (req, res) => {
+  const websiteId = req.params.websiteId
+  // Here you would typically check the status of the website with the given ID
+  // For now, we will just return a dummy status
+  return res.json({
+    "msg": "On /status",
+    websiteId: websiteId,
+    status: "up" // This is a placeholder; actual status checking logic would go here
+  })
+})
+
 
 
 app.listen(PORT, () => {
