1d451183b3e426d212be87b7994826c71134ef8c
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-08-03 21:57:29 +0500

    redis folder name changed to avoid recursive dependency

--- apps/pusher/index.ts
+++ apps/pusher/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { prismaClient } from "store/client";
+import { xAddBulk } from "redisstream/client";
+
+async function main() {
+  let websites = await prismaClient.website.findMany({
+    select: {
+      url: true,
+      id: true
+    }
+  })
+
+  await xAddBulk(websites)
+}
+
+setInterval(() => main(), 3 * 1000)
---
+++ packages/redisstream/index.ts
@@ -0,0 +1,58 @@
+import { createClient } from "redis";
+
+const client = await createClient()
+  .on("error", (err) => console.error("Redis Client Error:", err))
+  .connect();
+
+
+type WebsiteEvent = { url: string, id: string }
+
+
+async function xAdd({ url, id }: WebsiteEvent) {
+  try {
+    await client.xAdd(
+      "betteruptime:website", "*", {
+      url,
+      id
+    });
+  } catch (error) {
+    // Handle error (e.g., log or rethrow)
+    console.error("Failed to add to stream:", error);
+    throw error;
+  }
+}
+
+export async function xAddBulk(websites: WebsiteEvent[]) {
+  for (const { url, id } of websites) {
+    await xAdd({
+      url: url,
+      id: id
+    })
+  }
+}
+
+
+// More optimized approach
+export async function xAddBulkPipeline(websites: WebsiteEvent[]) {
+
+  // Start a Redis pipeline using MULTI
+  // This queues all the following commands to be sent in one batch.
+  // Redis processes them sequentially after .exec() is called.
+  const pipeline = client.multi()
+
+  // Add each website event to the Redis stream in the pipeline queue
+  for (const { url, id } of websites) {
+    pipeline.xAdd("betteruptime:website", "*", { url, id })
+  }
+
+  try {
+
+    // Execute all queued commands in a single request to Redis
+    // Returns an array of responses or errors for each command
+    await pipeline.exec()
+    console.log(`Added ${websites.length} events to the stream`)
+  } catch (err) {
+    console.error("Failed to add events to Redis stream in bulk:", err)
+    throw err;
+  }
+}
